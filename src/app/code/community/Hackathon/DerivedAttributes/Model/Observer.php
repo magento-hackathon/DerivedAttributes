e1f0d785073dd55b6eafebfb45d46a343ed13851
Gerrit Addiks <gerrit.addiks@brille24.de>
2015-05-09 15:29:28 +0200

    added config-xml

---
+++ src/app/code/community/Hackathon/DerivedAttributes/Model/Observer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Gerrit Addiks <gerrit.addiks@brille24.de>
+ */
+
+/**
+ * Event-observer for derived attributes.
+ */
+class Hackathon_DerivedAttributes_Model_Observer{
+    
+}

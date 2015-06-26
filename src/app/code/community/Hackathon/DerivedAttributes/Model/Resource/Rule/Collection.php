4186f613e88ec1974197556171b780a8699d2dca
Fabian Schmengler <fs@integer-net.de>
2015-06-26 23:38:46 +0200

    On second thought, let's keep the model

--- src/app/code/community/Hackathon/DerivedAttributes/Model/Resource/Rule/Collection.php
+++ src/app/code/community/Hackathon/DerivedAttributes/Model/Resource/Rule/Collection.php
@@ -3,16 +3,10 @@
  * @author Gerrit Addiks <gerrit.addiks@brille24.de>
  */
 
-/**
- *
- */
 class Hackathon_DerivedAttributes_Model_Resource_Rule_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
 {
-    //TODO move Hackathon_DerivedAttributes_Model_Rule so that it cannot be instantiated with Mage::getModel()
-    // it should only ever be instantiated by resource model and collection
-    protected $_itemObjectClass = 'Hackathon_DerivedAttributes_Model_Rule';
     protected function _construct()
     {
-        $this->_init(null, 'derivedattributes/rule');
+        $this->_init(Hackathon_DerivedAttributes_Model_Rule::ALIAS);
     }
 }
